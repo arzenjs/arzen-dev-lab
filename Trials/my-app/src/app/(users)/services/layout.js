8a8e78348aba901da3c97412ac3b8709032e39ea
Arzen <mohammadarsalan02013@gmail.com>
2026-05-09 18:52:44 +0530

    Removed outdated beginner projects

--- Trials/my-app/src/app/(users)/services/layout.js
+++
@@ -1,8 +0,0 @@
-export default function ServicesLayout({ children }) {
-    return (
-        <main className="flex-1">
-            <h1 className="text-4xl font-bold text-white-600 mb-8 text-center m-10 py-6 px-4 bg-blue-500 rounded-lg shadow-md">Our Services</h1>
-            {children}
-        </main>
-    );
-}
